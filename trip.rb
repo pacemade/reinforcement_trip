11159edea64e8a08ebefc99af874b5cc866a0818
Danny Chang <dannychachang@gmail.com>
2018-01-04 13:52:29 -0500

    Switch loop from each_with_index to each_cons()

--- trip.rb
+++ trip.rb
@@ -14,15 +14,24 @@ class Trip
     @places << destination
   end
 
+  # def begin_trip
+  #   p "Began trip"
+  #   @places.each_with_index do |place, i|
+  #     next_item = @places[i+1].name unless i == @places.size - 1
+  #     if i == @places.size - 1
+  #       p "Finished trip!"
+  #     else
+  #       p "Travelled from #{place.name} to #{next_item}"
+  #     end
+  #   end
+  # end
+
   def begin_trip
     p "Began trip"
-    @places.each_with_index do |place, i|
-      next_item = @places[i+1].name unless i == @places.size - 1
-      if i == @places.size - 1
-        p "Finished trip!"
-      else
-        p "Travelled from #{place.name} to #{next_item}"
-      end
+    @places.each_cons(2) do |place1, place2|
+        p "Travelled from #{place1.name} to #{place2.name}"
     end
+    p "Finished trip!"
   end
+
 end
